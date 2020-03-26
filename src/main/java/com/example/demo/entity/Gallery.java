f3c3f083b4c3ab13ffd4791a0f72580508341e2b
PMTuan <phamminhtuan@unimarmores.com>
2020-03-26 11:57:02 +0700

    update code

---
+++ src/main/java/com/example/demo/entity/Gallery.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gallery {
+
+  private Integer id;
+  private List<Object> images;
+
+}
