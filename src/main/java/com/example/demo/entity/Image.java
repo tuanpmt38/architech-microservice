e18c2b88c1026c3ab786e3d804b8592b340ec7f0
PMTuan <phamminhtuan@unimarmores.com>
2020-03-26 11:59:43 +0700

    new code service discover

---
+++ src/main/java/com/example/demo/entity/Image.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+  private Integer id;
+  private String title;
+  private String url;
+
+}
